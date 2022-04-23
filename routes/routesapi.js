1a1bc25b8e2b6fe5eba3545485a7863ab2a33205
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-04-23 15:38:29 -0400

    Added Get and POSt on routesapi.js

--- routes/routesapi.js
+++ routes/routesapi.js
@@ -1,1 +1,22 @@
 const fs = require("fs")
+const app = require("express").Router()
+let db = require("../db/db.json");
+
+
+app.get("/notes", function (req, res) {
+db = JSON.parse(fs.readFileSync("./db/db.json"))
+console.log("Get Route", db)
+res.json(db)
+})
+
+app.post("/notes", function (req, res) {
+    var id = Math.floor(Math.random()*101)
+    db.push({
+        id:id,
+        title:req.body.title,
+        text:req.body.text
+    })
+   fs.writeFileSync("./db/db.json", JSON.stringify(db))
+    console.log("POST Route", db)
+    res.json(db)
+    })
